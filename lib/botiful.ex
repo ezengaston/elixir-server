7f522fb486200fcfab5ae7a2d52e5df918e4b577
Ezen <ezen.gaston@gmail.com>
2023-06-27 12:54:07 +0300

    Initial commit

---
+++ lib/botiful.ex
@@ -0,0 +1,18 @@
+defmodule Botiful do
+  @moduledoc """
+  Documentation for `Botiful`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Botiful.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
